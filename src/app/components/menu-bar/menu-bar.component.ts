1fcfa65c0019f20f16ce6cb1adcc6f3c46fcf366
Eliezer Palhão <eliezer.ramosp@gmail.com>
2023-09-23 13:55:22 -0300

    BP-4 Alterado a pagina home para exibir o componente small-card dinamicamente usando o ngFor

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { data } from 'src/app/data/data';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+  projects:any[] = data.filter(project => project.standard === false)
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    //this.projects = data.filter(project => project.standard === false)
+    //console.log(this.projects)
+}
 }
